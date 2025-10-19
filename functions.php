3eb5df828c52b14c1704a9af88813f41069a8096
Denis <denistoday@gmail.com>
2025-10-20 01:20:03 +0200

    Страница Цены

--- functions.php
+++ functions.php
@@ -178,7 +178,7 @@ add_action('wp_enqueue_scripts', function(){
 		wp_enqueue_style('page-home', $uri.'pages/home.css', ['theme-utilities'], $ver);
 	}
 	
-	if (is_page(['about', 'reviews', 'contacts', 'team', 'o-klinike', 'otzyvy', 'kontakty', 'komanda'])) {
+	if (is_page(['about', 'reviews', 'contacts', 'team', 'prices', 'o-klinike', 'otzyvy', 'kontakty', 'komanda', 'tseny'])) {
 		wp_enqueue_style('page-inner', $uri.'pages/inner.css', ['theme-utilities'], $ver);
 		wp_enqueue_style('page-home', $uri.'pages/home.css', ['theme-utilities'], $ver);
 		wp_enqueue_script('theme-lightbox', get_stylesheet_directory_uri() . '/assets/js/lightbox.js', [], $ver, true);
